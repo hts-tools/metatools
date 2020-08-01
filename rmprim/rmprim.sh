bdddce34aa2941a2d4ee223127c2fe5787435cc1
Nico Salmaso <52832652+hts-tools@users.noreply.github.com>
2020-08-01 19:24:59 +0200

    Update rmprim.sh

--- rmprim/rmprim.sh
+++ rmprim/rmprim.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 # rmprim.sh
-# Function rmprim 0.1 / Bash script
+# Function rmprim 0.11 / Bash script
 # https://github.com/hts-tools/metatools/tree/master/rmprim
 # Author: Nico Salmaso, FEM, nico.salmaso@fmach.it
 # usage: bash rmprim.sh [arguments]
-# arguments: - f forward sequence; -r reverse sequence; -t discard untrimmed (TRUE/FALSE); -d directory
-while getopts f:r:t:d: option
+# arguments: - f forward sequence; -r reverse sequence; -n anchored (TRUE/FALSE); -t discard untrimmed (TRUE/FALSE); -d directory
+while getopts f:r:n:t:d: option
 do
 case "${option}"
 in
 f) FWD=${OPTARG};;
 r) REV=${OPTARG};;
+n) ANC=${OPTARG};;
 t) DIS=${OPTARG};;
 d) DIR=${OPTARG};;
 esac
 done
 echo "Forward: " $FWD
 echo "Reverse: " $REV
+echo "Anchored: " $ANC
 echo "Discard: " $DIS
 echo "Input dir: " $DIR
+    if [ "$ANC" = "TRUE" ]
+    then
+        anchored="^"
+    elif [ "$ANC" = "FALSE" ]
+    then
+        anchored=""
+    else
+        echo "bad -n value; -n should be TRUE or FALSE"
+        exit 0
+    fi
     if [ "$DIS" = "TRUE" ]
     then
         discard="--discard-untrimmed"
@@ -34,7 +46,7 @@ ls *_R1_001.fastq.gz | cut -d"_" -f1-3  > file_names
 for file_name in $(cat file_names)
     do
         echo "Reads trimmed on: $file_name"
-        cutadapt -g ^$FWD -G ^$REV \
+        cutadapt -g $anchored$FWD -G $anchored$REV \
         -o ${file_name}_R1_001_trim.fastq.gz -p ${file_name}_R2_001_trim.fastq.gz \
         ${file_name}_R1_001.fastq.gz ${file_name}_R2_001.fastq.gz \
         --cores=0 $discard -e 0.1 \
@@ -45,4 +57,3 @@ head trimming_statistic.txt
 echo ""
 echo "Open trimming_statistic.txt for complete results"
 echo ""
-
